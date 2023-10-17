c5e56014e79d1bfa75f941da8f551e4d1e8a23e2
Bhargava <vibhor.b@cs101u-5c.acad.fullerton.edu>
2023-10-17 08:49:16 -0700

    made order

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,7 +1,7 @@
 // David Diaz
 // ddiazjr929@csu.fullerton.edu
 // @Dav-929
-// Partners @vibhorhb
+// Partners: @vibhorbh
 
 #include <iostream>
 #include <string>
@@ -12,15 +12,22 @@ int main(int argc, char* argv[]) {
 
   // TODO: Validate that the number of arguments is correct.
   // If not, print an error message and return a non-zero value.
-
+if(arguments.size()<3){
+  std::cout << "error, exiting"<< "\n";
+  return 1;
+}
   // TODO: Declare three std::string variables to hold the
   // protein, bread, and condiment input.
   // Initialize each variable with an element of the arguments vector
   // declared above.
+  std::string protein{arguments[1]};
+  std::string bread{arguments[2]};
+  std::string condiment{arguments[3]};
 
   // TODO: Use cout to print output following the pattern
   // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
   // on its own line.
-
+std::cout << "your order: A " << protein << " sandwich on " 
+<< bread << " with " << condiment; 
   return 0;
 }
